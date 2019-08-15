a86756f4be0cb81599e65a227889debc2dd6826d
Zhiyuan Yin <zyin@google.com>
2019-08-15 16:57:22 -0700

    chapter 5

--- clion/moderncpptutorialch4/main.cpp
+++ clion/moderncpptutorialch4/main.cpp
@@ -3,7 +3,6 @@
 #include <unordered_map>
 
 int main() {
-
     std::unordered_map<int, std::string> u = {
         {1, "foo"},
         {2, "bar"}
---
+++ clion/moderncpptutorialch5/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+
+struct Foo {
+  Foo() { std::cout << "Foo::Foo" << std::endl; }
+  ~Foo() { std::cout << "Foo::~Foo" << std::endl; }
+  void foo() { std::cout << "Foo::foo" << std::endl; }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    std::unique_ptr<Foo> p1 = std::make_unique<Foo>();
+
+
+    if (p1) p1->foo();
+
+    {
+        std::unique_ptr<Foo> p2(std::move(p1));
+        if (p2) p2->foo();
+        auto p3 = std::move(p2);
+        p1 = std::move(p3);
+    }
+
+    if (p1) std::cout << "p1 has value!" << std::endl;
+
+    return 0;
+}
